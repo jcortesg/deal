7e3acd025afcedfb4b3b5d496f2d2d57ae811e6c
Jhon Cortes <jhone.cortesg@gmail.com>
2013-02-19 19:38:28 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gritalo::Application.config.secret_token = 'bf3749c15338cfdf3eaab6d601edc9dded70b108e0967db4e8cfdb454ce39da3bf372a45bb02c1425b187673123cef83ca55e1da15404fd02d9d6275ec3b93c7'
